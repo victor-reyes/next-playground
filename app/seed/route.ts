b2746f41cf029ca4f69ec86744eaddefe33b889b
Víctor Reyes <unhill@gmail.com>
2024-09-24 08:44:56 +0200

    Format code

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,8 +103,7 @@
 
 export async function GET() {
   return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    message: 'Uncomment this file and remove this line. You can delete this file when you are finished.',
   });
   // try {
   //   await client.sql`BEGIN`;
