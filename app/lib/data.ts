b129af347053bcdb6851efd5a99aef54ef10d583
Víctor Reyes <unhill@gmail.com>
2024-09-25 11:26:21 +0200

    Add a function to delay randomly

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -213,3 +213,12 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+async function delayRandomly(minDelay: number = 0, maxDelay: number = 1000) {
+  if (minDelay < 0 || maxDelay < 0) throw new Error('minDelay and maxDelay must be non-negative');
+
+  if (minDelay > maxDelay) throw new Error('minDelay cannot be greater than maxDelay.');
+
+  const delay = minDelay + Math.random() * (maxDelay - minDelay);
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
